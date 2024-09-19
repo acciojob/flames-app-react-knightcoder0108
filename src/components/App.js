adb840b13f59487ee94101ec85e0507d473c9f71
knightcoder0108 <ssharma.aabhi@gmail.com>
2024-09-19 16:40:35 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,49 +1,91 @@
-import React, { useState } from "react";
-import './../styles/App.css';
-import { v4 as uuidv4 } from 'uuid';
+import React, { useState } from 'react'
+
+const outputs = ["Friends", "Love", "Affection", "Marriage", "Enemy", "Siblings"]
 
 const App = () => {
 
-  const[todos, setTodos] = useState([])
-  const[text, setText] = useState("")
+    const[fn, setFn] = useState("")
+    const[ln, setLn] = useState("")
+    const[relation, setRelation] = useState("")
 
-  function deleteBtnHandler(){
 
-  }
 
+    const calculateBtnHandler = () => {
+
+        if(fn.length == 0 || ln.length == 0)
+        {
+            return setRelation("Please Enter valid input")
+        }
+
+        console.log("heheh")
+        let firstArr = fn.split("")
+        let SecondArr = ln.split("")
+        let nArr = []
+
+        for(let i = 0; i < firstArr.length; i++)
+        {
+            let flag = false;
+            for(let j = 0; j < SecondArr.length; j++)
+            {
+                if(firstArr[i] === SecondArr[j])
+                {
+                    flag = true;
+                    SecondArr.splice(j, 1)
+                    break;
+                }
+
+                
+            }
+
+            if(!flag)
+                {
+                    nArr.push(firstArr[i])
+                }
+        }
+
+        let tbc = (nArr.length + SecondArr.length) % 6;
+
+        switch(tbc){
+            case 1 :
+                return setRelation('Friends');
+            case 2 :
+                return setRelation('Love');
+            case 3 :
+                return setRelation('Affection');
+            case 4 :
+                return setRelation('Marriage');
+            case 5 :
+                return setRelation('Enemy');
+            case 0 :
+                return setRelation('Siblings');
+        }
+        
+    }
 
   return (
     <div>
-      <h3>To-Do List</h3>
-        <input value={text} onChange={(e) => {
-          setText(e.target.value)
-        }} />
-        <button onClick={(e) => {
-          if(!text) return
+        
+        <input name='name1' data-testid="input1" value={fn} onChange={(e) => {
+            setFn(e.target.value)
+        }} placeholder='Enter first name'/>
+        <input name='name2' data-testid="input2" value={ln} onChange={(e) => {
+            setLn(e.target.value)
+        }} placeholder='Enter second name'/>
+        <button data-testid="calculate_relationship" onClick={calculateBtnHandler}>Calculate Relationship Future</button>
+        <button data-testid="clear" onClick={() => {
+            setFn("")
+            setLn("")
+            setRelation("")
+        }}>Clear</button>
 
-          const newTodo = {
-            task : text,
-            id : uuidv4()
-          }
+        <h3 data-testid="answer">{relation}</h3>
 
-          setTodos([...todos, newTodo])
-
-          setText("")
-        }}>Add Todo</button>
-
-        <ul>
-          {todos && todos.map((item) => {
-            return <li style={{listStyle : "none"}} key={item.id}>{item.task}  <button onClick={() => {
-              const narr = todos.filter((todo) => {
-                return todo.id != item.id
-              })
-
-              setTodos(narr)
-            }}>Delete</button></li>
-          })}
-        </ul>
     </div>
   )
 }
 
 export default App
+
+
+
+//kjshdkjhsdf
